a0b6e7efe3d31adb48f544832cfae9da59c991d1
lcayment <lucie.cayment@gmail.com>
2020-11-23 09:33:21 +0100

    test2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace ProjetPendu
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenue sur notre jeu de pendu");
-	    Console.WriteLine("Prout");
+	    	Console.WriteLine("Prout");
+	    	Console.WriteLine("Prout2");
         }
     }
 }

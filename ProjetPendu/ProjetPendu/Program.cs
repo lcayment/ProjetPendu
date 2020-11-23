564113db20f6699c9dcb77275477d5bbedb94808
lcayment <lucie.cayment@gmail.com>
2020-11-23 08:33:38 +0100

    create sln and main

---
+++ ProjetPendu/ProjetPendu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetPendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue sur notre jeu de pendu");
+        }
+    }
+}
